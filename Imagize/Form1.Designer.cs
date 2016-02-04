51e15aaa8abec2437e258517228614eddc644371
Dherar AlRashoud <webmaster@q8geeks.org>
2016-02-04 16:35:13 +0300

    First build!

---
+++ Imagize/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Imagize
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.OFD = new System.Windows.Forms.OpenFileDialog();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.SFD = new System.Windows.Forms.SaveFileDialog();
+            this.btnImagize = new System.Windows.Forms.Button();
+            this.lblWarning = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // OFD
+            // 
+            this.OFD.FileName = "openFileDialog1";
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(12, 12);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(212, 38);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load File";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnImagize
+            // 
+            this.btnImagize.Location = new System.Drawing.Point(12, 199);
+            this.btnImagize.Name = "btnImagize";
+            this.btnImagize.Size = new System.Drawing.Size(212, 42);
+            this.btnImagize.TabIndex = 1;
+            this.btnImagize.Text = "IMAGIZE";
+            this.btnImagize.UseVisualStyleBackColor = true;
+            this.btnImagize.Visible = false;
+            this.btnImagize.Click += new System.EventHandler(this.btnImagize_Click);
+            // 
+            // lblWarning
+            // 
+            this.lblWarning.ForeColor = System.Drawing.Color.Red;
+            this.lblWarning.Location = new System.Drawing.Point(12, 53);
+            this.lblWarning.Name = "lblWarning";
+            this.lblWarning.Size = new System.Drawing.Size(212, 143);
+            this.lblWarning.TabIndex = 2;
+            this.lblWarning.Text = "The program would act as if its freezing, its not. Its just processing.\r\n\r\nI shou" +
+    "ld add an indicator. I just didn\'t get around that yet. Future releases will do." +
+    "";
+            this.lblWarning.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(236, 253);
+            this.Controls.Add(this.lblWarning);
+            this.Controls.Add(this.btnImagize);
+            this.Controls.Add(this.btnLoad);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Imagize";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog OFD;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.SaveFileDialog SFD;
+        private System.Windows.Forms.Button btnImagize;
+        private System.Windows.Forms.Label lblWarning;
+    }
+}
+
---
+++ Imagize/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace Imagize
+{
+    public partial class Form1 : Form
+    {
+        string FilePath = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OFD.Filter = "Any file (*.*)|*.*";
+            OFD.FileName = "";
+            DialogResult DR = OFD.ShowDialog();
+
+            if (DR == DialogResult.OK)
+            {
+                FilePath = OFD.FileName.ToString();
+                lblWarning.Visible = true;
+                btnImagize.Visible = true;
+            }
+        }
+
+        void GenerateImage()
+        {
+            FileInfo FI = new FileInfo(FilePath);
+            long FileSize = 0;
+            long PaddingSize = 0;
+            long ArraySize = 0;
+            double SizeX = 0;
+            double SizeY = 0;
+
+
+            FileSize = FI.Length;
+            while ((Math.Sqrt(FileSize + PaddingSize) %1) != 0)
+            {
+                PaddingSize++;
+            }
+
+            ArraySize = FileSize + PaddingSize;
+
+
+
+            SizeX = Math.Sqrt(ArraySize);
+            SizeY = SizeX;
+
+            byte[] TotalArray = new byte[ArraySize];
+            byte[] PaddingArray = new byte[PaddingSize];
+            byte[] FileBytes = File.ReadAllBytes(FilePath);
+
+            for (int x = 0; x < PaddingSize; x++)
+                PaddingArray[x] = (byte) 255;
+
+            for (int i = 0; i < FileSize; i++)
+                TotalArray[i] = FileBytes[i];
+            
+            for(long x = 0; x < (PaddingSize); x++)
+                TotalArray[FileSize + x] = PaddingArray[x];
+
+            long ByteCounter = 0;
+
+            Bitmap BM = new Bitmap((int)SizeX, (int)SizeY/3);
+            for (int cX = 0; cX < (int)SizeX; cX++)
+            {
+                for (int cY = 0; cY < (int) SizeY/3; cY++)
+                {
+                    Color nC = new Color();
+                    long R = 0;
+                    long G = 0;
+                    long B = 0;
+                    if (ByteCounter < (ArraySize - 1))
+                        R = ByteCounter;
+                    else
+                        R = (ArraySize - 1);
+
+                    if ((ByteCounter + 1) < (ArraySize - 1))
+                        G = ByteCounter + 1;
+                    else
+                        G = (ArraySize - 1);
+
+                    if ((ByteCounter + 2) < (ArraySize - 1))
+                        B = ByteCounter + 2;
+                    else
+                        B = (ArraySize - 1);
+
+                    nC = Color.FromArgb((int)TotalArray[R], (int)TotalArray[G], (int)TotalArray[B]);
+                    BM.SetPixel(cX, cY, nC);
+                    ByteCounter += 3;
+                }
+            }
+
+            SFD.Filter = "Bitmap (*.bmp)|*.bmp";
+            SFD.FileName = "";
+            DialogResult DR = SFD.ShowDialog();
+            if (DR == DialogResult.OK)
+            {
+                BM.Save(SFD.FileName.ToString());
+                for (int i = 0; i < TotalArray.Length; i++)
+                    TotalArray[i] = 0;
+                for (int i = 0; i < PaddingArray.Length; i++)
+                    PaddingArray[i] = 0;
+                for (int i = 0; i < FileBytes.Length; i++)
+                    FileBytes[i] = 0;
+                BM.Dispose();
+                MessageBox.Show("File successfully saved in:\n" + SFD.FileName, "Done!",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+            else
+            {
+                for (int i = 0; i < TotalArray.Length; i++)
+                    TotalArray[i] = 0;
+                for (int i = 0; i < PaddingArray.Length; i++)
+                    PaddingArray[i] = 0;
+                for (int i = 0; i < FileBytes.Length; i++)
+                    FileBytes[i] = 0;
+                BM.Dispose();
+                MessageBox.Show("Operation Cancelled.\n\nErasing loaded data.", "Cancelled!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnImagize_Click(object sender, EventArgs e)
+        {
+            GenerateImage();
+        }
+    }
+}
